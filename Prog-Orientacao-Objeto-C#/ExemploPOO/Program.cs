141ef6ed66cb805b00ce2f3a0d90ff53f8681f82
Michel Tsukiyama <michel.tsukiyama@gmail.com>
2022-01-01 21:53:49 -0300

    adiciona Prog-Orientacao-Objeto

---
+++ Prog-Orientacao-Objeto-C#/ExemploPOO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ExemploPOO.Helper;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caminho = "D:\\DCM";
+
+            FileHelper helper = new FileHelper();
+            // helper.ListarDirectory(caminho);
+            helper.ListFiles(caminho);
+
+            // Aluno p1 = new Aluno();
+            // p1.Nome = "Bob"; 
+            // p1.Nota = 20;
+            // p1.Apresentar();
+
+            
+            // Retangulo r = new Retangulo();
+
+            // r.DefinirMedidas(30,30);
+            // System.Console.WriteLine($"Área: {r.ObterArea()}");
+
+
+            // Pessoa p1 = new Pessoa();
+
+            // p1.Nome = "Bob";
+            // p1.Idade = 20;
+
+            // p1.Apresentar();
+        }
+    }
+}
